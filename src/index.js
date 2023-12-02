c9be09f198d2fbd7d26aed32d06eea5a5012b690
matryoshka <nazardjali@gmail.com>
2023-12-03 00:44:48 +0900

    Add hiding login btns after dom loaded
    But flashing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { dep } from "./main";
-import authClient from "./authClient";
+import authClient, {isAuthenticated} from "./authClient";
 
 authClient.init()
 console.log("New auth status: ", authClient.isAuthenticated ? "Logged in" : "Anonymous")
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginBtnsContainer = document.getElementById('login-btns-container')
+    if (isAuthenticated()) {
+        loginBtnsContainer.style.display = 'none'
+    }
+})
